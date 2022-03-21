cb4054c3c9feca427853adaca525bc9a29e38b23
Corina_MC <98157883+corinamc@users.noreply.github.com>
2022-03-21 21:40:28 +0000

    Add files via upload
    My first few lines

---
+++ my_first_code.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# this is my first line of code
+
+
+# In[2]:
+
+
+print('My name is Corina')
+print('The date is 21st March 2022')
+
+
+# In[3]:
+
+
+# Write a program to analyse real estate data
+
+
+# In[4]:
+
+
+# create variables
+a = 45000
+b = 23400
+c = 67000
+d = 34600
+e = 12900
+print(a,b,c,d,e)
+
+
+# In[5]:
+
+
+print(a+b+c+d+e)
+
+
+# In[7]:
+
+
+# calculate average sales
+print((a+b+c+d+e)/5)
+
+
+# In[8]:
+
+
+# calculate property price (saled price - commission) for property c
+c -=6500
+print (c)
+
+
+# In[ ]:
+
+
+
+
